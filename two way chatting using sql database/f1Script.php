5007f9d68df7841de35bf44717baa5cac81b1aa1
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-14 16:02:00 +0530

    Update f1Script.php

--- two way chatting using sql database/f1Script.php
+++ two way chatting using sql database/f1Script.php
@@ -1,23 +1,23 @@
 <?php
     
-    $servername = "x";
-    $username = "x";
-    $password = "x";
-    $database="x";
-    $mysqli = new mysqli($servername, $username, $password, $database);
-    $mysqli->select_db($database) or die( "Unable to select database");
+$servername = "x";
+$username = "x";
+$password = "x";
+$database="x";
+$mysqli = new mysqli($servername, $username, $password, $database);
+$mysqli->select_db($database) or die( "Unable to select database");
 $msg=array();
 	
-	$tt=$_POST['msg'];
-    $tsql= "INSERT INTO `dbo.f1` (msg) VALUES ('$tt')"; 
+$tt=$_POST['msg'];
+$tsql= "INSERT INTO `dbo.f1` (msg) VALUES ('$tt')"; 
 
-	if ($result = $mysqli -> query($tsql)) {
-    	$msg['msg']="done";
-	}else{
-		$msg['msg']="err";
-	}
-	
-	
-	echo  json_encode($msg);   
+if ($result = $mysqli -> query($tsql)) {
+	$msg['msg']="done";
+}else{
+	$msg['msg']="err";
+}
+
+
+echo  json_encode($msg);   
 
 ?>
