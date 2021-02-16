6717ab4057d16d4e782addc2f06ab24ab100e7b8
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-16 16:08:44 +0530

    Update ssedemo.php

--- server sent events/ssedemo.php
+++ server sent events/ssedemo.php
@@ -1,36 +1,9 @@
 <?php
   
-  session_start();
 header("Content-Type: text/event-stream");
 header("Cache-Control: no-cache");
   
   echo "retry: 1000\n\n"; 
-  echo "data: " . " no message " . "\n\n"; 
+  echo "data: " . " no message " . "\n\n"; //message which will be sent 
   flush();  
-
-  
-//echo "retry: 500\n\n";  
-//echo "data: "." msg form server ";  
-//flush(); 
-       
-
-/* if(isset($_SESSION['msg'])){
-    
-  echo "retry: 1000\n\n"; 
-  echo "data: " . "message" . "\n\n"; 
-  flush(); 
- 
-
- }
- else{
-    
-  echo "retry: 1000\n\n"; 
-  echo "data: " . "no messager" . "\n\n"; 
-  flush(); 
-} */
- 
-  
-
-
- 
 ?> 
